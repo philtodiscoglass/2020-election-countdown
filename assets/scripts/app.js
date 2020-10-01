12a024549dd16507aa79e8e3248d2075ac47a72e
Phil Todisco <philtodiscoglass@gmail.com>
2020-09-30 23:11:28 -0400

    Countdown Functional Displays correct data onto screen using jQuery Displays message when countdown is over using jQuery Added some basic HTML

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,24 +2,39 @@
 
 function createCountdown () {
   // set the date (Washington DC time)
-let electionDay = new Date('Nov 3, 2020 00:00:00')
+  let electionDay = new Date('Nov 3, 2020 00:00:00')
   // returns time in ms since UNIX epoch, then convert to seconds
   electionDay = (Date.parse(electionDay) / 1000)
 
   // set current time, return time in ms since UNIX epoch
   let currentTime = new Date()
-  currentTime= (Date.parse(currentTime) / 1000)
+  currentTime = (Date.parse(currentTime) / 1000)
 
   // time between electionDay and currentTime in seconds
-  let remainingTime = electionDay - currentTime
+  const remainingTime = electionDay - currentTime
 
   // calculations for days/hours/minutes/seconds using Math.floor() which "returns the largest integer less than or equal to a given number"
   // days = seconds / 86400
   // hours = seconds / 3600
   // minutes = seconds / 60
-  let days = Math.floor(remainingTime / 86400);
-  let hours = Math.floor((remainingTime - (days * 86400)) / 3600)
-  let minutes = Math.floor((remainingTime - (days * 86400) - (hours * 3600)) / 60)
-  let seconds = Math.floor((remainingTime - (days * 86400) - (hours * 3600) - (minutes * 60)))
+  const days = Math.floor(remainingTime / 86400)
+  const hours = Math.floor((remainingTime - (days * 86400)) / 3600)
+  const minutes = Math.floor((remainingTime - (days * 86400) - (hours * 3600)) / 60)
+  const seconds = Math.floor((remainingTime - (days * 86400) - (hours * 3600) - (minutes * 60)))
+
+  // displays countdown info on the page using jQuery .html
+  $('#days').html(days + '<span>Days</span>')
+  $('#hours').html(hours + '<span>Hours</span>')
+  $('#minutes').html(minutes + '<span>Minutes</span>')
+  $('#seconds').html(seconds + '<span>Seconds</span>')
+
+  // display message when countdown is over using .html
+  if (remainingTime <= 0) {
+    $('#message').html('<span>Countdown Over!</span>')
+  }
 }
-createCountdown()
+
+// call function every second
+setInterval(function () {
+  createCountdown()
+}, 1000)
