a9a1839bb2f4dacc668e5ec2e4dec3f221b249a8
Phil Todisco <philtodiscoglass@gmail.com>
2020-09-30 22:49:05 -0400

    Calculated Days/Hours/Minutes/Seconds Calculated and tested starting from seconds between electionDay and currentDate

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,25 @@
 'use strict'
 
-function createCountdown() {
-
+function createCountdown () {
   // set the date (Washington DC time)
 let electionDay = new Date('Nov 3, 2020 00:00:00')
   // returns time in ms since UNIX epoch, then convert to seconds
   electionDay = (Date.parse(electionDay) / 1000)
 
-  // set current time, return time in ms since UNIX epoch, then convert to seconds
+  // set current time, return time in ms since UNIX epoch
   let currentTime = new Date()
-  currentTime = (Date.parse(currentTime) / 1000)
+  currentTime= (Date.parse(currentTime) / 1000)
+
+  // time between electionDay and currentTime in seconds
+  let remainingTime = electionDay - currentTime
+
+  // calculations for days/hours/minutes/seconds using Math.floor() which "returns the largest integer less than or equal to a given number"
+  // days = seconds / 86400
+  // hours = seconds / 3600
+  // minutes = seconds / 60
+  let days = Math.floor(remainingTime / 86400);
+  let hours = Math.floor((remainingTime - (days * 86400)) / 3600)
+  let minutes = Math.floor((remainingTime - (days * 86400) - (hours * 3600)) / 60)
+  let seconds = Math.floor((remainingTime - (days * 86400) - (hours * 3600) - (minutes * 60)))
 }
+createCountdown()
